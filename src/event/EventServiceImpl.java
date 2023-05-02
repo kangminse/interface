18da6a6f9c0ba64f2420e73619a8e41c456b12b4
test1 <test@a.a>
2023-05-02 13:13:12 +0900

    이벤트완성

--- src/event/EventServiceImpl.java
+++ src/event/EventServiceImpl.java
@@ -1,5 +1,17 @@
 package event;
 
-public class EventServiceImpl  {
+public class EventServiceImpl implements EventService {
+
+	@Override
+	public void monthEvent() {
+		System.out.println("이달의 이벤트");
+	}
+
+	@Override
+	public void totalEvent() {
+		System.out.println("전체 이벤트");
+
+	}
+	
 	
 }
